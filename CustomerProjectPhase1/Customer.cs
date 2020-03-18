4af868f88900ff38d82300d5a7ac3623da9da67f
mehtar100 <32291121+mehtar100@users.noreply.github.com>
2020-03-18 13:02:08 +1300

    Add project files.

---
+++ CustomerProjectPhase1/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InterfaceCustomer;
+namespace MiddleLayer
+{
+   
+    public class Customer : CustomerBase
+    {
+        
+        public Customer(IValidation<ICustomer> obj, 
+                            IDiscount dis,
+                            IExtraCharge extra,
+                            string _CustType) : base(obj,dis,extra)
+        {
+            CustomerType = _CustType;
+            CustomerName = "";
+            PhoneNumber = "";
+            BillAmount = 0;
+            BillDate = DateTime.Now;
+            Address = "";
+        }
+        
+    }
+   
+}
